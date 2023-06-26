65505eb3f0c951361a2817734011d613fe5dffcb
yongj.zhuang <yongj.zhuang@outlook.com>
2023-06-26 14:01:05 +0800

    Optional index.html

--- main.go
+++ main.go
@@ -17,9 +17,34 @@ import (
 var (
 	mu        sync.RWMutex
 	mem_store map[string][]byte = make(map[string][]byte)
+	indexHtml []byte            = nil
 )
 
 func main() {
+
+	var err error
+	indexHtml, err = os.ReadFile("./index.html")
+	if err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
+
+	if indexHtml != nil {
+		server.RawGet("/", func(c *gin.Context, ec common.ExecContext) {
+			c.Header("Content-Type", "text/html")
+			_, _ = c.Writer.Write(indexHtml)
+		})
+	}
+
+	server.Get("/file/list", func(c *gin.Context, ec common.ExecContext) (any, error) {
+		mu.RLock()
+		defer mu.RUnlock()
+		var keys []string = []string{}
+		for k := range mem_store {
+			keys = append(keys, k)
+		}
+		return keys, nil
+	})
+
 	server.RawPost("/file/:file", func(c *gin.Context, ec common.ExecContext) {
 		file := c.Param("file")
 		mu.Lock()
